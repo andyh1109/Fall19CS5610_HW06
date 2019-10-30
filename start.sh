4a32c907512991d74737e8ec580335796283f6d4
Timesheets1 App <time1@guest.guest>
2019-10-30 21:04:12 +0000

    sync

--- start.sh
+++ start.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-export MIX_ENV=prod
-export PORT=4892
+source ./prod-env.sh
 
-echo "Starting app..."
-
-# Start to run in background from shell.
-#_build/prod/rel/memory/bin/memory start
-
-# Foreground for testing and for systemd
-_build/prod/rel/memory/bin/memory start
-
-# TODO: Add a systemd service file
-#       to start your app on system boot.
-
+_build/prod/rel/timesheets/bin/timesheets start
